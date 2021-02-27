0b14611b5b530e74a70e917beba2d27800ac619f
alefepdias@gmail.com <alefepdias@gmail.com>
2021-02-27 12:44:12 -0400

    Inserido a pasta > components views

--- src/App.js
+++ src/App.js
@@ -1,35 +1,13 @@
 import React from 'react'
+import 'bootswatch/dist/flatly/bootstrap.css'
+import Cadastro from './views/cadastroUsuario'
 
 class App extends React.Component {
-
-  state = {
-    numero1 : null,
-    numero2 : null,
-    resultado : null
-  }
-
-  somar = () => {
-    const resultado = parseInt(this.state.numero1) + parseInt(this.state.numero2)
-    this.setState({resultado: resultado})
-  }
-
   render(){
     return(
       <div>
-        <label>Primeiro número</label>
-          <input type="text" value={this.state.numero1} 
-          onChange={(e) => this.setState({numero1: e.target.value}) } />
-          <br />
-          <label>Segundo número</label>
-          <input type="text" value={this.state.numero2} 
-          onChange={(e) => this.setState({numero2: e.target.value}) } />
-          <br />
-          <button onClick={this.somar}>
-            Somar
-          </button>
-
-          <br />
-          A soma dos 2 numeros é: {this.state.resultado}
+        <Cadastro />
+        <formGroup />
       </div>
     )
   }
