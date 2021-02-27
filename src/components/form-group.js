7e9bc9138567e73befb947643cf35f5f9bc61eb9
alefepdias@gmail.com <alefepdias@gmail.com>
2021-02-27 15:27:43 -0400

    Pagina de cadastro criada

--- src/components/form-group.js
+++ src/components/form-group.js
@@ -1,14 +1,12 @@
 import React from 'react'
 
-class formGroup extends React.Component{
-    render(){
-        return(
-            <div className="form-group">
-                <label for="exampleInputEmail1">Nome: *</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Digite o Nome"></input>
-            </div>
-        )
-    }
+function FormGroup(props) {
+    return (
+        <div className="form-group">
+            <label htmlFor={props.htmlFor}>{props.label}</label>
+            {props.children}
+        </div>
+    )
 }
 
-export default formGroup
+export default FormGroup
--- src/views/cadastroUsuario.js
+++ src/views/cadastroUsuario.js
@@ -1,22 +1,61 @@
 import React from 'react'
-import formGroup from '../components/form-group'
+import Card from '../components/card'
+import FormGroup from '../components/form-group'
 
 class Cadastro extends React.Component{
+
+    state = {
+        nome: '',
+        email: '',
+        senha: '',
+        senhaRepeticao: ''
+    }
+
+    cadastrar = () => {
+        console.log(this.state)
+    }
     render(){
-            return(
+        return(
             <div className="container">
-                <div className="row">
-                    <div className="col-lg-12">
-                        <div className="bs-component">
-                            <form>
-                                <fieldset>
-                                    <formGroup />
-                                </fieldset>
-                            </form>
+                <Card title="Pagina de cadastro">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="bs-component">
+                                <FormGroup label="Nome: *" htmlFor="inputNome">
+                                    <input type="text" 
+                                    id="inputNome" 
+                                    className="form-control"
+                                    name="nome"
+                                    onChange={e => this.setState({nome: e.target.value})}/>
+                                </FormGroup>
+                                <FormGroup label="Email: *" htmlFor="inputEmail">
+                                    <input type="text" 
+                                    id="inputEmail" 
+                                    className="form-control"
+                                    name="email"
+                                    onChange={e => this.setState({email: e.target.value})}/>
+                                </FormGroup>
+                                <FormGroup label="Senha: *" htmlFor="inputSenha">
+                                    <input type="password" 
+                                    id="inputSenha" 
+                                    className="form-control"
+                                    name="senha"
+                                    onChange={e => this.setState({senha: e.target.value})}/>
+                                </FormGroup>
+                                <FormGroup label="Repita senha: *" htmlFor="inputRepitaSenha">
+                                    <input type="password" 
+                                    id="inputRepitaSenha" 
+                                    className="form-control"
+                                    name="senha"
+                                    onChange={e => this.setState({senhaRepeticao: e.target.value})}/>
+                                </FormGroup>
+                                <button onClick={this.cadastrar} type="button" className="btn btn-success">Salvar</button>
+                                <button type="button" className="btn btn-danger">Voltar</button>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
+            </Card>
+            </div>             
         )
     }
 }
