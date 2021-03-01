e3c8d6ea60ef7cef5242b69848c0e72b593f7755
Willian Maltus <willianmaltus@bol.com.br>
2021-03-01 17:20:05 -0300

    tela de login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import 'bootswatch/dist/flatly/bootstrap.css'
 import Cadastro from './views/cadastroUsuario'
+import Login from './views/login'
 
 class App extends React.Component {
   render(){
     return(
       <div>
+        <Login />
         <Cadastro />
       </div>
     )
